08576239ea99cc249ab76660c40ee86dca8495fe
eva <evaorange73@gmail.com>
2023-09-07 21:24:21 +0300

    ReadWriteLock

--- src/main/server/src/main/java/control/CollectionManager.java
+++ src/main/server/src/main/java/control/CollectionManager.java
@@ -5,6 +5,9 @@ import data.MusicBand;
 import java.util.Date;
 import java.util.LinkedHashSet;
 import java.util.TreeSet;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * Менеджер коллекции -- класс, осуществляющий работу с коллекцией:
@@ -16,26 +19,115 @@ public class CollectionManager {
     private final Date creationDate = new Date();
     private final DatabaseManager databaseManager;
 
+    private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    private final Lock readLock = readWriteLock.readLock();
+    private final Lock writeLock = readWriteLock.writeLock();
+
     public CollectionManager(DatabaseManager databaseManager) {
         this.databaseManager = databaseManager;
     }
 
+    // Методы записи
+
     public void setCollection(LinkedHashSet<MusicBand> musicBands) {
-        collection = musicBands;
-        this.sort();
+        writeLock.lock();
+        try {
+            collection = musicBands;
+            this.sort();
+        } finally {
+            writeLock.unlock();
+        }
+    }
+
+    public void add(MusicBand musicBand) {
+        writeLock.lock();
+        try {
+            Integer id = databaseManager.insertAndGetId(musicBand);
+            musicBand.setId(id);
+            collection.add(musicBand);
+            this.sort();
+        } finally {
+            writeLock.unlock();
+        }
+    }
+
+    public void update(MusicBand oldMusicBand, MusicBand newMusicBand) {
+        writeLock.lock();
+        try {
+            databaseManager.update(oldMusicBand.getId(), newMusicBand);
+            collection.remove(oldMusicBand);
+            collection.add(newMusicBand);
+            this.sort();
+        } finally {
+            writeLock.unlock();
+        }
+    }
+
+    public void remove(int musicBandId) {
+        writeLock.lock();
+        try {
+            databaseManager.removeMusicBand(musicBandId);
+            this.collection.removeIf(musicBand -> musicBand.getId() == (musicBandId));
+        } finally {
+            writeLock.unlock();
+        }
+    }
+
+    // Методы чтения
+
+    public LinkedHashSet<MusicBand> getCollection() {
+        readLock.lock();
+        try {
+            return this.collection;
+        } finally {
+            readLock.unlock();
+        }
     }
 
     public MusicBand getById(Integer id) {
-        for (MusicBand musicBand : this.collection) {
-            if (musicBand.getId().equals(id)) {
-                return musicBand;
+        readLock.lock();
+        try {
+            for (MusicBand musicBand : this.collection) {
+                if (musicBand.getId().equals(id)) {
+                    return musicBand;
+                }
             }
+            return null;
+        } finally {
+            readLock.unlock();
         }
-        return null;
     }
 
-    public LinkedHashSet<MusicBand> getCollection() {
-        return this.collection;
+    public MusicBand getMaxElement() {
+        readLock.lock();
+        try {
+            return this.collection.iterator().next();
+        } finally {
+            readLock.unlock();
+        }
+    }
+
+    public MusicBand getMinElement() {
+        readLock.lock();
+        try {
+            MusicBand maxMusicBand = null;
+            for (MusicBand musicBand : this.collection)
+                maxMusicBand = musicBand;
+            return maxMusicBand;
+        } finally {
+            readLock.unlock();
+        }
+    }
+
+    // Остальные
+
+    public int getSize() {
+        readLock.lock();
+        try {
+            return this.collection.size();
+        } finally {
+            readLock.unlock();
+        }
     }
 
     public String getType() {
@@ -44,40 +136,6 @@ public class CollectionManager {
 
     public Date getCreationDate() {
         return this.creationDate;
-    }
-
-    public int getSize() {
-        return this.collection.size();
-    }
-
-    public void remove(int musicBandId) {
-        databaseManager.removeMusicBand(musicBandId);
-        this.collection.removeIf(musicBand -> musicBand.getId() == (musicBandId));
-    }
-
-    public void add(MusicBand musicBand) {
-        Integer id = databaseManager.insertAndGetId(musicBand);
-        musicBand.setId(id);
-        collection.add(musicBand);
-        this.sort();
-    }
-
-    public void update(MusicBand oldMusicBand, MusicBand newMusicBand) {
-        databaseManager.update(oldMusicBand.getId(), newMusicBand);
-        collection.remove(oldMusicBand);
-        collection.add(newMusicBand);
-        this.sort();
-    }
-
-    public MusicBand getMaxElement() {
-        return this.collection.iterator().next();
-    }
-
-    public MusicBand getMinElement() {
-        MusicBand maxMusicBand = null;
-        for (MusicBand musicBand : this.collection)
-            maxMusicBand = musicBand;
-        return maxMusicBand;
     }
 
     private void sort() {
